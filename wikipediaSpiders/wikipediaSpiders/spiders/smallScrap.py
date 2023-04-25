5908a9f4a6e97a230669d9480350b5bd9fd80b55
Adam Vickerman <svadam15@gmail.com>
2023-04-25 15:32:38 -0500

    Fix to '\n' split and removal
    Switch to converting News and Did you know var. to string then splitting with .split('\n')

--- wikipediaSpiders/wikipediaSpiders/spiders/smallScrap.py
+++ wikipediaSpiders/wikipediaSpiders/spiders/smallScrap.py
@@ -9,7 +9,6 @@ class firstScrap(scrapy.Spider):
 
     def parse(self, response):
 
-        testHolder = []
         skimThrough = response.css('#mp-upper')
 
         for dialog in skimThrough:
@@ -22,11 +21,10 @@ class firstScrap(scrapy.Spider):
 
             # "Did you know" section
             dykOutput = dialog.xpath('/html/body/div[2]/div/div[3]/main/div[3]/div[3]/div[1]/div[2]/div[1]/div[2]/ul').getall()
-            dykOutput = dykOutput[0].split('\n',1) # Splits the list from being one big list into smaller elements similar to how it is on the website
-            for x in range(len(dykOutput)):
-                dykOutput[x] = dykOutput[x].replace('\n','')
-                dykOutput[x] = w3lib.html.remove_tags(dykOutput[x])
+            dykOutput = [w3lib.html.remove_tags(dykOutput[x]) for x in range(len(dykOutput))] # Removes all HTML tags from list
 
+            dykOutput = dykOutput[0] # Converts it to a string to allow for '\n' to be removed
+            dykOutput = dykOutput.split('\n') # Removes "\n" from all of the elements and returns it to a list
             
 
             # "In the news" section *Mistake in the jsonl file happening where a break to a new element isn't happening
@@ -39,29 +37,18 @@ class firstScrap(scrapy.Spider):
 
             # New style
             newsOutput = dialog.xpath('/html/body/div[2]/div/div[3]/main/div[3]/div[3]/div[1]/div[2]/div[2]/div[1]/ul').getall()
-            # newsOutput = newsOutput[0].split('<li>',1) # Splits the list from being one big list into smaller elements similar to how it is on the website
             newsOutput = [w3lib.html.remove_tags(newsOutput[x]) for x in range(len(newsOutput))] # Removes all HTML tags from list
-            # newsOutput = [x.replace('\n','') for x in newsOutput] # Removes "\n" from all of the elements
+            newsOutput = newsOutput[0] # Converts it to a string to allow for '\n' to be removed
+            newsOutput = newsOutput.split('\n') # Removes "\n" from all of the elements and returns it to a list
 
-            holdvar = 0
-            changeNews = newsOutput
-            for x in range(newsOutput[0].count("\n")):
-                testHolder.append(changeNews[0][0:changeNews[0].index('\n')])
-                changeNews = changeNews[0][changeNews[0].index('\n'):]
-                # print(testHolder)
-
-                
-            # for x in range(len(newsOutput)):
-            #     newsOutput[x] = newsOutput[x].replace('\n','')
-            #     newsOutput[x] = w3lib.html.remove_tags(newsOutput[x])
             
 
             
             yield {
                 "Today's date": date.today(),
                 "Today's featured article": featuredOutput,
-                # "Did you know": dykOutput,
-                "In the news": testHolder
+                "Did you know": dykOutput,
+                "In the news": newsOutput
             }
         
         
